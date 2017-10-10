65f3516f27eb3635fca1de429545c51c18e1dcf8
Joseph Chen <josephchen817@gmail.com>
2017-10-10 14:02:47 -0500

    Add dplyr examples

---
+++ bit001_dplyr-cheatsheet.R
@@ -0,0 +1,46 @@
+## http://stat545.com/bit001_dplyr-cheatsheet.html
+
+library(readr)
+
+superheroes <- "
+    name, alignment, gender,         publisher
+ Magneto,       bad,   male,            Marvel
+   Storm,      good, female,            Marvel
+Mystique,       bad, female,            Marvel
+  Batman,      good,   male,                DC
+   Joker,       bad,   male,                DC
+Catwoman,       bad, female,                DC
+ Hellboy,      good,   male, Dark Horse Comics
+"
+superheroes <- read_csv(superheroes, trim_ws = TRUE, skip = 1)
+
+publishers <- "
+  publisher, yr_founded
+         DC,       1934
+     Marvel,       1939
+      Image,       1992
+"
+publishers <- read_csv(publishers, trim_ws = TRUE, skip = 1)
+
+## inner_join(superheroes, publishers)
+(ijsp <- inner_join(superheroes, publishers))
+(ijps <- inner_join(publishers, superheroes))
+
+## semi_join(superheroes, publishers)
+(sjsp <- semi_join(superheroes, publishers))
+(sjps <- semi_join(x = publishers, y = superheroes))
+
+## left_join(superheroes, publishers)
+(ljsp <- left_join(superheroes, publishers))
+(ljps <- left_join(publishers, superheroes))
+
+## anti_join(superheroes, publishers)
+(ajsp <- anti_join(superheroes, publishers))
+(ajps <- anti_join(publishers, superheroes))
+
+## full_join(superheroes, publishers)
+(fjsp <- full_join(superheroes, publishers))
+
+library(devtools)
+devtools::session_info()
+#> Session info -------------------------
---
+++ block009_dplyr-intro.R
@@ -0,0 +1,18 @@
+## ref: http://stat545.com/block009_dplyr-intro.html
+
+## Meet the new pipe operator
+gapminder %>% head()
+
+## Use filter() to subset data row-wise
+filter(gapminder, lifeExp < 29)
+filter(gapminder, country == "Rwanda", year > 1979)
+filter(gapminder, country %in% c("Rwanda", "Afghanistan"))
+
+## Use select() to subset the data on variables or columns
+select(gapminder, year, lifeExp)
+gapminder %>%
+  select(year, lifeExp) %>%
+  head(4)
+gapminder %>%
+  filter(country == "Cambodia") %>%
+  select(year, lifeExp)
