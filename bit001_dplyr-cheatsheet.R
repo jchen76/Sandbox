d7a0cfd1dee256fd641bfb1555cd68e78852add9
Joseph Chen <jchen@reddyice.com>
2018-02-25 08:07:31 -0600

    Update with definition of joins

--- bit001_dplyr-cheatsheet.R
+++ bit001_dplyr-cheatsheet.R
@@ -24,22 +24,30 @@ publishers <- "
 publishers <- read_csv(publishers, trim_ws = TRUE, skip = 1)
 
 ## inner_join(superheroes, publishers)
+# inner_join(x, y): Return all rows from x where there are matching values in y,
+# and all columns from x and y.
 (ijsp <- inner_join(superheroes, publishers))
 (ijps <- inner_join(publishers, superheroes))
 
 ## semi_join(superheroes, publishers)
+# semi_join(x, y): Return all rows from x where there are matching values in y,
+# keeping just columns from x.
 (sjsp <- semi_join(superheroes, publishers))
 (sjps <- semi_join(x = publishers, y = superheroes))
 
 ## left_join(superheroes, publishers)
+# left_join(x, y): Return all rows from x, and all columns from x and y.
 (ljsp <- left_join(superheroes, publishers))
 (ljps <- left_join(publishers, superheroes))
 
 ## anti_join(superheroes, publishers)
+# anti_join(x, y): Return all rows from x where there are not matching values in y,
+# keeping just columns from x.
 (ajsp <- anti_join(superheroes, publishers))
 (ajps <- anti_join(publishers, superheroes))
 
 ## full_join(superheroes, publishers)
+# full_join(x, y): Return all rows and all columns from both x and y.
 (fjsp <- full_join(superheroes, publishers))
 
 library(devtools)
